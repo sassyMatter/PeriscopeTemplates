aafe0b027641a24f41ccf8ff1251357a17a33c97
Nirbhay <nirbhay11.singh@ril.com>
2023-06-13 13:13:47 +0530

    added buildTree method to create tree from canvas Data

--- src/main/java/com/app/models/canvasSchema/TreeNode.java
+++ src/main/java/com/app/models/canvasSchema/TreeNode.java
@@ -2,10 +2,15 @@ package com.app.models.canvasSchema;
 
 import com.app.models.canvas.CanvasObject;
 import com.app.models.canvas.Connection;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TreeNode {
+@Getter
+@Setter
+public class  TreeNode {
 
     public CanvasObject data;
 
@@ -13,6 +18,11 @@ public class TreeNode {
 
      public TreeNode(CanvasObject data){
         this.data = data;
+        this.connections = new ArrayList<>();
+    }
+
+    public void addChild(TreeNode node){
+         connections.add(node);
     }
 
 }
--- src/main/java/com/app/services/TreeBuilderService.java
+++ src/main/java/com/app/services/TreeBuilderService.java
@@ -2,20 +2,49 @@ package com.app.services;
 
 import com.app.models.canvas.CanvasData;
 import com.app.models.canvas.CanvasObject;
+import com.app.models.canvas.Connection;
 import com.app.models.canvasSchema.TreeNode;
 import com.sun.source.tree.Tree;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Service
+@Slf4j
 public class TreeBuilderService {
 
     public TreeNode buildTree(CanvasData objects){
 
         List<TreeNode> treeNodes = convertObjects(objects);
 //      populating child nodes for each node and setting up the root for the same
+//      Map to hold canvas objects
+        TreeNode root = null;
+        Map<String, TreeNode> treeNodeMap = new HashMap<>();
 
-        return null;
+        for(int i = 0; i < treeNodes.size(); i++){
+            TreeNode node = treeNodes.get(i);
+            treeNodeMap.put(node.getData().getId(), node);
+            if(node.getData().getType() == "input"){
+                root = node;
+            }
+        }
+
+        for(int i = 0; i < treeNodes.size(); i++){
+            TreeNode node = treeNodes.get(i);
+            List<Connection> connections = node.data.getConnections();
+
+            connections.forEach(id ->{
+               node.addChild(treeNodeMap.get(id));
+            });
+
+        }
+
+        return root;
     }
 
 
