42558b5a56d85576da56855b09d7788a1a8e0fa6
Nirbhay <nirbhay11.singh@ril.com>
2023-10-11 10:01:52 +0530

    CORS policy changes

--- src/main/java/com/app/controllers/AuthController.java
+++ src/main/java/com/app/controllers/AuthController.java
@@ -32,7 +32,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
---
+++ src/main/java/com/app/models/customModels/Dummy.java
@@ -0,0 +1,4 @@
+package com.app.models.customModels;
+
+public class Dummy {
+}
