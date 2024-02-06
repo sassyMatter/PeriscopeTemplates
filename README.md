a435aaad510a29142c9eea46397ef0aab02c26af
Nirbhay11 Singh <nirbhay11.Singh@ril.com>
2024-02-06 13:05:25 +0530

    changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Templates for periscope project
+
+1. Template contains a lot of core logic, final templates would be very lightweight with only few class and configuration files.
+
---
+++ src/main/java/com/app/controllers/UserEndpoints.java
@@ -0,0 +1,26 @@
+package com.app.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/converter")
+@CrossOrigin(origins = "*")
+@Slf4j
+public class UserEndpoints {
+
+
+
+
+
+
+
+
+
+
+
+  
+
+}
