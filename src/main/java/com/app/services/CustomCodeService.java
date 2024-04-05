f9211cd5076b8755792b156f5a22fe5cd1bedc0f
sangam12-08 <Sangam.Barnwal@ril.com>
2024-04-05 19:43:22 +0530

    chanegs

--- src/main/java/com/app/services/CustomCodeService.java
+++ src/main/java/com/app/services/CustomCodeService.java
@@ -7,8 +7,11 @@ import com.app.producers.KafkaProducer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Service
@@ -17,6 +20,11 @@ public class CustomCodeService {
 
     @Autowired
     KafkaProducer kafkaProducer;
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
 
 
 
